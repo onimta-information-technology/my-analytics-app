f5889fe73940aa444c9fef2a41c4121b6094aadf
Anushka Dilshan <87310590+AnushkaDilshan@users.noreply.github.com>
2025-08-21 09:50:33 +0530

    home screen last seen and username water mark created

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,6 +6,7 @@ import 'package:ballys_reservation_app/utils/storage_util.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:intl/intl.dart';
 
 class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({super.key});
@@ -16,7 +17,7 @@ class HomeScreen extends ConsumerStatefulWidget {
 
 class _HomeScreenState extends ConsumerState<HomeScreen> {
   String? userName;
-
+  DateTime? lastseen;
   @override
   void initState() {
     super.initState();
@@ -28,6 +29,7 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
     final name = await StorageUtil.getUserName();
     setState(() {
       userName = name;
+      lastseen = DateTime.now();
     });
   }
 
@@ -41,6 +43,9 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     final guests = ref.watch(guestsProvider);
+    final formattedLastSeen = lastseen != null
+        ? DateFormat('dd MMM yyyy, hh:mm a').format(lastseen!)
+        :'';
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -48,166 +53,234 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
           style: const TextStyle(fontSize: 16),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Expanded(
-                  child: GestureDetector(
-                    onTap: () async {
-                      // Navigator.of(context).push(MaterialPageRoute(
-                      //     builder: (context) => const MemberVisits(
-                      //           title: 'Today Member Visits',
-                      //         )));
-                      final userLevel = await StorageUtil.getUserLevel();
-                      if (userLevel == '1') {
-                        context.push('/home/sales-persons', extra: {
-                          'title': 'All Sales Persons (Today)',
-                          'salesPersons': groupByMGroup(guests.todayGuests)
-                        });
-                      } else {
-                        context.push('/home/member-visits', extra: {
-                          'title': 'Today Member Visits',
-                          'guestList': guests.todayGuests
-                        });
-                      }
-                    },
-                    child: Card(
-                      color: const Color.fromARGB(255, 228, 117, 14),
-                      child: Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Column(
-                          children: [
-                            Text(
-                              guests.todayGuests
-                                  .where((guest) => guest.mid.isNotEmpty)
-                                  .length
-                                  .toString(),
-                              style: const TextStyle(
-                                  fontSize: 50.0,
-                                  fontWeight: FontWeight.w500,
-                                  color: Colors.white),
-                            ),
-                            const Text(
-                              'Today',
-                              style: TextStyle(
-                                  fontSize: 16.0,
-                                  fontWeight: FontWeight.normal,
-                                  color: Colors.white),
-                            ),
-                          ],
-                        ),
-                      ),
+      body: Stack(
+        children: [
+          IgnorePointer(
+      child: Opacity(
+        opacity: 0.1,
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            final width = constraints.maxWidth;
+            final height = constraints.maxHeight;
+
+            // create a grid of watermark texts
+            return Wrap(
+              alignment: WrapAlignment.center,
+              runAlignment: WrapAlignment.center,
+              spacing: 80,
+              runSpacing: 80,
+              children: List.generate(
+                50, // number of watermarks
+                (index) => Transform.rotate(
+                  angle: -0.5,
+                  child: Text(
+                    userName != null
+                        ? '$userName\n$formattedLastSeen'
+                        : 'Loading...',
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
                     ),
                   ),
                 ),
-                // SizedBox(width: 16),
-                Expanded(
-                  child: GestureDetector(
-                    onTap: () async {
-                      final userLevel = await StorageUtil.getUserLevel();
-                      if (userLevel == '1') {
-                        Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) => SalesPersonsScreen(
+              ),
+            );
+          },
+        ),
+      ),
+    ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: GestureDetector(
+                        onTap: () async {
+                          // Navigator.of(context).push(MaterialPageRoute(
+                          //     builder: (context) => const MemberVisits(
+                          //           title: 'Today Member Visits',
+                          //         )));
+                          final userLevel = await StorageUtil.getUserLevel();
+                          if (userLevel == '1') {
+                            context.push(
+                              '/home/sales-persons',
+                              extra: {
+                                'title': 'All Sales Persons (Today)',
+                                'salesPersons': groupByMGroup(
+                                  guests.todayGuests,
+                                ),
+                              },
+                            );
+                          } else {
+                            context.push(
+                              '/home/member-visits',
+                              extra: {
+                                'title': 'Today Member Visits',
+                                'guestList': guests.todayGuests,
+                              },
+                            );
+                          }
+                        },
+                        child: Card(
+                          color: const Color.fromARGB(255, 228, 117, 14),
+                          child: Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Column(
+                              children: [
+                                Text(
+                                  guests.todayGuests
+                                      .where((guest) => guest.mid.isNotEmpty)
+                                      .length
+                                      .toString(),
+                                  style: const TextStyle(
+                                    fontSize: 50.0,
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                const Text(
+                                  'Today',
+                                  style: TextStyle(
+                                    fontSize: 16.0,
+                                    fontWeight: FontWeight.normal,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    // SizedBox(width: 16),
+                    Expanded(
+                      child: GestureDetector(
+                        onTap: () async {
+                          final userLevel = await StorageUtil.getUserLevel();
+                          if (userLevel == '1') {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) => SalesPersonsScreen(
                                   title: 'All Sales Persons (Yesterday)',
-                                  salesPersons:
-                                      groupByMGroup(guests.yesterdayGuests),
-                                )));
-                      } else {
-                        Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) => MemberVisits(
+                                  salesPersons: groupByMGroup(
+                                    guests.yesterdayGuests,
+                                  ),
+                                ),
+                              ),
+                            );
+                          } else {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) => MemberVisits(
                                   title: 'Yesterday Member Visits',
                                   guestList: guests.yesterdayGuests,
-                                )));
-                      }
-                    },
-                    child: Card(
-                      color: Colors.green,
-                      child: Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Column(
-                          children: [
-                            Text(
-                              guests.yesterdayGuests
-                                  .where((guest) => guest.mid.isNotEmpty)
-                                  .length
-                                  .toString(),
-                              style: const TextStyle(
-                                  fontSize: 50.0,
-                                  fontWeight: FontWeight.w500,
-                                  color: Colors.white),
+                                ),
+                              ),
+                            );
+                          }
+                        },
+                        child: Card(
+                          color: Colors.green,
+                          child: Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Column(
+                              children: [
+                                Text(
+                                  guests.yesterdayGuests
+                                      .where((guest) => guest.mid.isNotEmpty)
+                                      .length
+                                      .toString(),
+                                  style: const TextStyle(
+                                    fontSize: 50.0,
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                const Text(
+                                  'Yesterday',
+                                  style: TextStyle(
+                                    fontSize: 16.0,
+                                    fontWeight: FontWeight.normal,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ],
                             ),
-                            const Text(
-                              'Yesterday',
-                              style: TextStyle(
-                                  fontSize: 16.0,
-                                  fontWeight: FontWeight.normal,
-                                  color: Colors.white),
-                            ),
-                          ],
+                          ),
                         ),
                       ),
                     ),
-                  ),
+                  ],
                 ),
-              ],
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: GestureDetector(
-                    onTap: () async {
-                      final userLevel = await StorageUtil.getUserLevel();
-                      if (userLevel == '1') {
-                        Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) => SalesPersonsScreen(
+                Row(
+                  children: [
+                    Expanded(
+                      child: GestureDetector(
+                        onTap: () async {
+                          final userLevel = await StorageUtil.getUserLevel();
+                          if (userLevel == '1') {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) => SalesPersonsScreen(
                                   title: 'All Sales Persons (Monthly)',
-                                  salesPersons:
-                                      groupByMGroup(guests.monthlyGuests),
-                                )));
-                      } else {
-                        Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) => MemberVisits(
+                                  salesPersons: groupByMGroup(
+                                    guests.monthlyGuests,
+                                  ),
+                                ),
+                              ),
+                            );
+                          } else {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) => MemberVisits(
                                   title: 'Monthly Member Visits',
                                   guestList: guests.monthlyGuests,
-                                )));
-                      }
-                    },
-                    child: Card(
-                      color: const Color.fromARGB(255, 42, 125, 192),
-                      child: Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Column(
-                          children: [
-                            Text(
-                              guests.monthlyGuests
-                                  .where((guest) => guest.mid.isNotEmpty)
-                                  .length
-                                  .toString(),
-                              style: const TextStyle(
-                                  fontSize: 50.0,
-                                  fontWeight: FontWeight.w500,
-                                  color: Colors.white),
+                                ),
+                              ),
+                            );
+                          }
+                        },
+                        child: Card(
+                          color: const Color.fromARGB(255, 42, 125, 192),
+                          child: Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Column(
+                              children: [
+                                Text(
+                                  guests.monthlyGuests
+                                      .where((guest) => guest.mid.isNotEmpty)
+                                      .length
+                                      .toString(),
+                                  style: const TextStyle(
+                                    fontSize: 50.0,
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                const Text(
+                                  'Monthly',
+                                  style: TextStyle(
+                                    fontSize: 16.0,
+                                    fontWeight: FontWeight.normal,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ],
                             ),
-                            const Text(
-                              'Monthly',
-                              style: TextStyle(
-                                  fontSize: 16.0,
-                                  fontWeight: FontWeight.normal,
-                                  color: Colors.white),
-                            ),
-                          ],
+                          ),
                         ),
                       ),
                     ),
-                  ),
+                  ],
                 ),
               ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }

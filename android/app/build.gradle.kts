f92795394c3efb90cbce5a7d5dbbde2b04a47838
Tharindu Liyanagama <tharinduliyanagama@Tharindus-Mac-mini.local>
2025-11-20 08:35:31 +0530

    androd configeration

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,34 +10,39 @@ android {
     namespace = "com.app.ballys_reservation"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
-
+    
     compileOptions {
         // isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-
+    
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
-
+    
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.app.ballys_reservation"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
-        // multiDexEnabled = true
     }
-
+    
+    signingConfigs {
+        create("release") {
+            keyAlias = "MyAnalytics"
+            keyPassword = "onimtait"
+            storeFile = file("MyAnalytics.jks")
+            storePassword = "onimtait"
+        }
+    }
+    
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = false
+            isShrinkResources = false
         }
     }
 }
@@ -49,5 +54,4 @@ flutter {
 dependencies {
     implementation("com.google.firebase:firebase-messaging:23.4.0")
     implementation("com.google.android.gms:play-services-auth-api-phone:18.0.1")
-    //coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }

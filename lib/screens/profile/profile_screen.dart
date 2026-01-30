7859b3c25419400f16984498f50801a27a089bf7
Tharindu Liyanagama <tharinduliyanagama@Tharindus-Mac-mini.local>
2026-01-30 17:11:14 +0530

    gift screen fix

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -468,7 +468,11 @@ class _ProfileScreenState extends ConsumerState<ProfileScreen>
         FocusScope.of(context).unfocus();
       },
       child: Scaffold(
-        appBar: AppBar(title: const Text("Guest Profile")),
+        appBar: AppBar(
+            leading: IconButton(
+              icon: const Icon(Icons.arrow_back),
+              onPressed: () => context.pop(),
+            ),title: const Text("Guest Profile")),
         body: Stack(
           children: [
             PopScope(
